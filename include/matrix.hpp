ebccef5e404fcee177211ea3001d5bed0b515ccb
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-03-20 17:01:55 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,13 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
 
-class matrix_t final
+using namespace std;
+
+class Matrix
 {
 private:
-    unsigned int rows_;
-    unsigned int columns_;
-    int** elements_;
+	int **matrix;
+	int Columns;
+	int Strings;
+
 public:
-    matrix_t() noexcept;
-    auto rows() -> unsigned int;
-    auto columns() -> unsigned int;
+	Matrix()
+	{
+
+	};
+
+	Matrix(int a, int b)
+	{
+		Columns = a;
+		Strings = b;
+		matrix = new int*[a];
+
+		for (int i = 0; i < a; i++)
+		{
+			matrix[i] = new int[b]{0};
+		}
+	}
+	Matrix(const Matrix&Matrix)
+	{
+		Columns = Matrix.Columns;
+		Strings = Matrix.Strings;
+		matrix = new int*[Columns];
+
+		for (int i = 0; i < Columns; i++)
+		{
+			matrix[i] = new int[Matrix.Strings];
+		}
+	}
 };
