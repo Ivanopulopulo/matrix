cea2ea4d87194900d4f84f267d201863b238af5b
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-03-20 18:40:38 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,27 +1,31 @@
 #include <matrix.hpp>
 #include <catch.hpp>
 
-SCENARIO("default constructor") {
+SCENARIO("default constructor")
+{
 	Matrix matrix;
-	REQUIRE(matrix.Strings() == 0);
-	REQUIRE(matrix.Columns() == 0);
+	REQUIRE(matrix.Strings_() == 0);
+	REQUIRE(matrix.Columns_() == 0);
 }
 
-SCENARIO("constructor with params") {
+SCENARIO("constructor with params") 
+{
 	Matrix matrix(3, 3);
-	REQUIRE(matrix.Strings() == 3);
-	REQUIRE(matrix.Columns() == 3);
+	REQUIRE(matrix.Strings_() == 3);
+	REQUIRE(matrix.Columns_() == 3);
 }
 
-SCENARIO("copy constructor") {
+SCENARIO("copy constructor") 
+{
 	Matrix matrix (3, 3);	
 	Matrix copy(matrix);	
-	REQUIRE(copy.Strings() == 3);
-	REQUIRE(copy.Columns() == 3);
+	REQUIRE(copy.Strings_() == 3);
+	REQUIRE(copy.Columns_() == 3);
 }
 
 
-SCENARIO("operator +") {
+SCENARIO("operator +") 
+{
 	Matrix matrix1 (3, 3);
 	Matrix matrix2 (3, 3);
 	Matrix matrix3matrix3 (3, 3);	
@@ -31,7 +35,8 @@ SCENARIO("operator +") {
 	REQUIRE(matrix1 + matrix2 == matrix3);
 }
 
-SCENARIO("operator *") {
+SCENARIO("operator *") 
+{
 	Matrix matrix1 (3, 3);
 	Matrix matrix2 (3, 3);
 	Matrix matrix3 (3, 3);	
@@ -40,7 +45,8 @@ SCENARIO("operator *") {
 	std::ifstream("multmatrix.txt") >> matrix3;
 	REQUIRE(matrix1 * matrix2 == matrix3);
 }
-SCENARIO("operator =") {
+SCENARIO("operator =") 
+{
 	Matrix matrix1(3, 3);
 	Matrix matrix2 = matrix1;
 	REQUIRE(matrix1 == matrix2);
