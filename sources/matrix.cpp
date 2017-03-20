3c85103e8cc354da7a9e6ce65df98742872f4e5e
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-03-20 18:27:58 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -1,97 +1,159 @@
 #include "matrix.hpp"
 
-	int Col() const
-	{
-		return Columns;
-	}
+int Matrix::Columns_()
+{
+	return Columns;
+}
 
-	int Str() const
-	{
-		return Strings;
-	}
-	int set(int x, int y, int z) const
-	{
-		return matrix[x][y] = z;
-	}
+int Matrix::Strings_()
+{
+	return Strings;
+}
 
-	int get(int x, int y) const
-	{
-		return matrix[x][y];
+Matrix::Matrix()
+{
+	Columns = 0;
+	Strings = 0;
+	matrix = new int*[Strings];
+	for (int i = 0; i < Strings; i++){
+		matrix[i] = new int[Columns];
 	}
-	void print(void) const
-	{
-		for (int i = 0; i < Columns; i++){
-			for (int j = 0; j < Strings; j++){
-				cout << " " << get(i, j) << " | ";
-			}
-			cout << endl;
+}
+
+Matrix::Matrix(int _Columns, int _Strings)
+{
+	Columns = _Columns;
+	Strings = _Strings;
+	matrix = new int*[Strings];
+	for (int i = 0; i < Strings; ++i){
+		matrix[i] = new int[Columns];
+		for (int j = 0; j < Columns; ++j){
+			matrix[i][j] = 0;
 		}
 	}
+}
 
-	void input(char *path) const
+Matrix::Matrix(const Matrix& result)
+{
+	Columns = result.Columns;
+	Strings = result.Strings;
+	matrix = new int*[Strings];
+	for (int i = 0; i < Strings; ++i){
+		matrix[i] = new int[Columns];
+		for (int j = 0; j < Columns; ++j){
+			matrix[i][j] = result.matrix[i][j];
+		}
+	}
+}
+
+Matrix::~Matrix()
+{
+	for (int i = 0; i < Strings; ++i){
+		delete[]matrix[i];
+	}
+	delete[]matrix;
+}
+
+istream& operator >> (istream& infile, const Matrix& result)
+{
+	for (int i = 0; i < result.Strings; i++)
+	for (int j = 0; j < result.Columns; j++)
+		infile >> result.matrix[i][j];
+	return infile;
+}
+
+void Matrix::scan(string filename)
+{
+	ifstream infile;
+	infile.open(filename);
+	if (!infile.is_open())
+		cout << "Error! Please, try again!" << endl;
+	else
 	{
-		ifstream file;
-		file.open(path);
-
-
-		for (int i = 0; i < Columns; i++)
-		{
-			for (int j = 0; j < Strings; j++)
-			{
-				char *temp = new char[5];
-				file.getline(temp, 5);
-				set(i, j, atoi(temp));
+		matrix = new int*[Strings];
+		for (int i = 0; i < Strings; i++){
+			matrix[i] = new int[Columns];
+			for (int j = 0; j < Columns; j++){
+				infile >> matrix[i][j];
 			}
 		}
 	}
+	infile.close();
+}
 
-	
-
-	Matrix Matrix& :: operator =(const Matrix &Matrix)
-	{
-		if (this!= &Matrix)
-		{
-			for (int i = 0; i<Strings; i++)
-				delete[]matrix[i];
-			delete[]matrix;
-			Columns = Matrix.Columns;
-			Strings = Matrix.Strings;
-			matrix = new int*[Columns];
-
-			for (int i = 0; i < Columns; i++)
-			{
-				matrix[i] = new int[Matrix.Strings];
-			}
-			matrix[Columns][Strings] = Matrix.matrix[Columns][Strings];
+ostream& operator << (ostream& outfile, const Matrix& result)
+{
+	for (int i = 0; i < result.Strings; i++){
+		for (int j = 0; j < result.Columns; j++){
+			outfile << result.matrix[i][j] << " ";
 		}
-		return *this;
 	}
+	outfile << endl;
+	return outfile;
+}
 
-	Matrix Matrix& :: operator +(Matrix a)
-	{
-		for (int i = 0; i < Columns; i++)
-		{
-			for (int j = 0; j < Strings; j++)
-			{
-				a.set(i, j, a.get(i, j) + get(i, j));
-			}
+bool Matrix::operator == (const Matrix& m2) const
+{
+	bool k = false;
+	for (int i = 0; i < Strings; i++){
+		for (int j = 0; j < Columns; j++){
+			if (matrix[i][j] == m2.matrix[i][j])
+				k = true;
 		}
-		return a;
 	}
-	Matrix Matrix :: operator *(Matrix a)
-	{
-		for (int i = 0; i < Columns; i++)
-		{
-			for (int j = 0; j < Strings; j++)
-			{
-				a.set(i, j, a.get(i, j) * get(i, j));
+	return k;
+}
+
+Matrix Matrix::operator + (const Matrix& m2) const
+{
+	if ((Columns != m2.Columns) || (Strings != m2.Strings)) {
+		cout << "Error!";
+	}
+	else {
+		Matrix result(Columns, Strings);
+		for (int i = 0; i < Strings; ++i){
+			for (int j = 0; j < Columns; ++j){
+				result.matrix[i][j] = matrix[i][j] + m2.matrix[i][j];
 			}
 		}
-		return a;
+		return result;
 	}
-	Matrix:: ~Matrix()
-	{
-		for (int i = 0; i<Strings; i++)
-			delete[]matrix[i];
-		delete[]matrix;
+}
+
+Matrix Matrix::operator * (const Matrix& m2) const
+{
+	if (m2.Strings != Columns){
+		cout << "Error!";
 	}
+	else {
+		Matrix result(Strings, m2.Columns);
+		for (int i = 0; i < Strings; i++){
+			for (int j = 0; j < m2.Columns; j++){
+				for (int k = 0; k < Columns; k++){
+					result.matrix[i][j] += matrix[i][k] * m2.matrix[k][j];
+				}
+			}
+		}
+		return result;
+	}
+}
+
+Matrix& Matrix::operator = (const Matrix& result)
+{
+	if (&result != this){
+		for (int i = 0; i < Strings; i++){
+			delete[] matrix[i];
+		}
+		delete[] matrix;
+	}
+	Strings = result.Strings;
+	Columns = result.Columns;
+	matrix = new int*[Strings];
+	for (int i = 0; i < Strings; i++){
+		matrix[i] = new int[Columns];
+		for (int j = 0; j < Columns; j++){
+			matrix[i][j] = result.matrix[i][j];
+		}
+	}
+	return *this;
+}
