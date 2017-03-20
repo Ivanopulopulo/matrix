98e9c0e63940d68364939ba5784e13f5a4dde7d7
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-03-20 17:27:29 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -13,31 +13,17 @@ private:
 	int Strings;
 
 public:
-	Matrix()
-	{
-
-	};
-
-	Matrix(int a, int b)
-	{
-		Columns = a;
-		Strings = b;
-		matrix = new int*[a];
-
-		for (int i = 0; i < a; i++)
-		{
-			matrix[i] = new int[b]{0};
-		}
-	}
-	Matrix(const Matrix&Matrix)
-	{
-		Columns = Matrix.Columns;
-		Strings = Matrix.Strings;
-		matrix = new int*[Columns];
-
-		for (int i = 0; i < Columns; i++)
-		{
-			matrix[i] = new int[Matrix.Strings];
-		}
-	}
+	Matrix();
+	
+	Matrix(int a, int b);
+	
+	Matrix(const Matrix&Matrix);
+	~Matrix();
+	
+	void print(void) const
+	void input(char *path) const;
+	Matrix& operator =(const Matrix &Matrix);
+	Matrix& operator +(Matrix a);
+	Matrix operator *(Matrix a);
+	
 };
