3c690d583b73c3810d61a5f2e07da4b476c9cb3f
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-03-20 17:43:44 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -3,15 +3,21 @@
 
 SCENARIO("default constructor") {
 	Matrix matrix;
+	REQUIRE(matrix.Strings() == 0);
+	REQUIRE(matrix.Columns() == 0);
 }
 
 SCENARIO("constructor with params") {
 	Matrix matrix(3, 3);
+	REQUIRE(matrix.Strings() == 3);
+	REQUIRE(matrix.Columns() == 3);
 }
 
 SCENARIO("copy constructor") {
 	Matrix matrix (3, 3);	
 	Matrix copy(matrix);	
+	REQUIRE(copy.Strings() == 3);
+	REQUIRE(copy.Columns() == 3);
 }
 
 
@@ -21,27 +27,22 @@ SCENARIO("operator +") {
 	Matrix matrix3matrix3 (3, 3);	
 	std::ifstream("matrix1.txt") >> matrix1;
 	std::ifstream("matrix2.txt") >> matrix2;
-	std::ifstream("summatrix.txt") >> matrix3matrix3;
+	std::ifstream("summatrix.txt") >> matrix3;
+	REQUIRE(matrix1 + matrix2 == matrix3);
 }
 
 SCENARIO("operator *") {
 	Matrix matrix1 (3, 3);
 	Matrix matrix2 (3, 3);
 	Matrix matrix3 (3, 3);	
-	std::ifstream("matrix1.txt") >> matrix1matrix1;
+	std::ifstream("matrix1.txt") >> matrix1;
 	std::ifstream("matrix2.txt") >> matrix2;
 	std::ifstream("multmatrix.txt") >> matrix3;
+	REQUIRE(matrix1 * matrix2 == matrix3);
 }
 SCENARIO("operator =") {
 	Matrix matrix1(3, 3);
 	Matrix matrix2 = matrix1;
+	REQUIRE(matrix1 == matrix2);
 }
-REQUIRE(matrix.Strings() == 0);
-REQUIRE(matrix.Columns() == 0);
-REQUIRE(matrix.Strings() == 3);
-REQUIRE(matrix.Columns() == 3);
-REQUIRE(copy.Strings() == 3);
-REQUIRE(copy.Columns() == 3);
-REQUIRE(matrix1 + matrix2 == matrix3);
-REQUIRE(matrix1 * matrix2 == matrix3);
-REQUIRE(matrix1 == matrix2);
+
