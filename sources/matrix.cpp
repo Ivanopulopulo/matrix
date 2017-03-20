abb8d6ff41e6c64490eb131c504456efbf519beb
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-03-20 17:00:43 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -1,15 +1,97 @@
 #include "matrix.hpp"
 
-matrix_t::matrix_t() noexcept : rows_{0}, columns_{0}, elements_{nullptr}
-{
-}
+int Col() const
+	{
+		return Columns;
+	}
 
-auto matrix_t::rows() -> unsigned int
-{
-    return rows_;
-}
+	int Str() const
+	{
+		return Strings;
+	}
+	int set(int x, int y, int z) const
+	{
+		return matrix[x][y] = z;
+	}
 
-auto matrix_t::columns() -> unsigned int
-{
-    return columns_;
-}
+	int get(int x, int y) const
+	{
+		return matrix[x][y];
+	}
+	void print(void) const
+	{
+		for (int i = 0; i < Columns; i++){
+			for (int j = 0; j < Strings; j++){
+				cout << " " << get(i, j) << " | ";
+			}
+			cout << endl;
+		}
+	}
+
+	void input(char *path) const
+	{
+		ifstream file;
+		file.open(path);
+
+
+		for (int i = 0; i < Columns; i++)
+		{
+			for (int j = 0; j < Strings; j++)
+			{
+				char *temp = new char[5];
+				file.getline(temp, 5);
+				set(i, j, atoi(temp));
+			}
+		}
+	}
+
+	
+
+	Matrix& operator =(const Matrix &Matrix)
+	{
+		if (this!= &Matrix)
+		{
+			for (int i = 0; i<Strings; i++)
+				delete[]matrix[i];
+			delete[]matrix;
+			Columns = Matrix.Columns;
+			Strings = Matrix.Strings;
+			matrix = new int*[Columns];
+
+			for (int i = 0; i < Columns; i++)
+			{
+				matrix[i] = new int[Matrix.Strings];
+			}
+			matrix[Columns][Strings] = Matrix.matrix[Columns][Strings];
+		}
+		return *this;
+	}
+
+	Matrix& operator +(Matrix a)
+	{
+		for (int i = 0; i < Columns; i++)
+		{
+			for (int j = 0; j < Strings; j++)
+			{
+				a.set(i, j, a.get(i, j) + get(i, j));
+			}
+		}
+		return a;
+	}
+	Matrix operator *(Matrix a)
+	{
+		for (int i = 0; i < Columns; i++)
+		{
+			for (int j = 0; j < Strings; j++)
+			{
+				a.set(i, j, a.get(i, j) * get(i, j));
+			}
+		}
+		return a;
+	}
+	Matrix:: ~Matrix()
+	{
+		for (int i = 0; i<Strings; i++)
+			delete[]matrix[i];
+		delete[]matrix;
+	}
